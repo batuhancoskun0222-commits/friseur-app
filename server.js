90fd6316b36026787a33b18922c17ca7349a0de1
batuhancoskun0222-commits <batuhancoskun0222@gmail.com>
2026-05-30 22:10:45 +0200

    datenbank variablen angepasst

--- server.js
+++ server.js
@@ -14,10 +14,11 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Batu2525',
-  database: 'terminVerwaltung'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect(err => {
